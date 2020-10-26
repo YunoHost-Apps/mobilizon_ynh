e186da6477d7d1eb44e50a32ffdd351154ff1ad8
Alexandre Aubin <alex.aubin@mailoo.org>
2020-10-26 19:48:54 +0100

    (to be validated...) Integrate mail .. which requires a dummy user

---
+++ conf/mail.exs
@@ -0,0 +1,18 @@
+
+config :mobilizon, Mobilizon.Web.Email.Mailer,
+  adapter: Bamboo.SMTPAdapter,
+  server: "127.0.0.1",
+  #hostname: "127.0.0.1",
+  # usually 25, 465 or 587
+  port: 25,
+  username: "__YNH_USER__",
+  password: "__YNH_USER_PASSWORD__",
+  # can be `:always` or `:never`
+  tls: :never,
+  allowed_tls_versions: [:"tlsv1.2"],
+  retries: 1,
+  # can be `true`
+  no_mx_lookups: false,
+  # can be `:always`. If your smtp relay requires authentication set it to `:always`.
+  auth: :always
+
