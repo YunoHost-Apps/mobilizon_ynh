a2c646d04cd0bb26da6c14a6a3319bdd24e5b2ae
ericgaspar <junk.eg@free.fr>
2021-12-23 09:08:37 +0100

    Remove elixir

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,9 +6,6 @@
 
 # dependencies used by the app
 pkg_dependencies="build-essential inotify-tools postgresql postgresql-client postgresql-contrib postgis postgresql-postgis-scripts git curl unzip gnupg openssl make gcc libc-dev argon2 imagemagick webp gifsicle jpegoptim optipng pngquant cmake"
-extra_pkg_dependencies="elixir esl-erlang"
-
-NODEJS_VERSION=14
 
 #=================================================
 # PERSONAL HELPERS
