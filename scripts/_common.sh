e2faf51d6796d3c886c41969f2a6ad9254d079b2
yalh76 <yalh@yahoo.com>
2022-01-18 21:03:36 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql postgresql-client postgresql-contrib postgis postgresql-postgis-scripts argon2 imagemagick webp gifsicle jpegoptim optipng pngquant"
+pkg_dependencies="postgresql postgresql-client postgresql-contrib postgis postgresql-postgis-scripts argon2 imagemagick webp gifsicle jpegoptim optipng pngquant file"
 
 #=================================================
 # PERSONAL HELPERS
