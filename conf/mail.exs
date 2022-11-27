50911b9022353a38a24a6439ea7b5aaca87921f7
Robles Rodolphe <rodolphe.robles@sfr.fr>
2022-11-27 23:16:41 +0100

    Try debug send email

--- conf/mail.exs
+++ conf/mail.exs
@@ -1,7 +1,9 @@
 
 config :mobilizon, Mobilizon.Web.Email.Mailer,
-  adapter: Swoosh.Adapters.SMTP,
-  relay: "127.0.0.1",
+  #adapter: Swoosh.Adapters.SMTP,
+  #relay: "127.0.0.1",
+  adapter: Bamboo.SMTPAdapter,
+  server: "127.0.0.1",
   #hostname: "127.0.0.1",
   # usually 25, 465 or 587
   port: 25,
