2d0663045d0ba5499f291e582fe16ee8f6776b2b
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-01 18:49:13 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="postgresql postgresql-client postgresql-contrib postgis postgresql-postgis-scripts postgresql-13-postgis-3 argon2 imagemagick webp gifsicle jpegoptim optipng pngquant file"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
@@ -18,3 +15,28 @@
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+read_json () {
+    sudo python3 -c "import sys, json;print(json.load(open('$1'))['$2'])"
+}
+
+read_manifest () {
+    if [ -f '../manifest.json' ] ; then
+        read_json '../manifest.json' "$1"
+    else
+        read_json '../settings/manifest.json' "$1"
+    fi
+}
+abort_if_up_to_date () {
+    version=$(read_json "/etc/yunohost/apps/$YNH_APP_INSTANCE_NAME/manifest.json" 'version' 2> /dev/null || echo '20160501-7')
+    last_version=$(read_manifest 'version')
+    if [ "${version}" = "${last_version}" ]; then
+        ynh_print_info "Up-to-date, nothing to do"
+        ynh_die "" 0
+    fi
+}
+
+ynh_version_gt ()
+{
+    dpkg --compare-versions "$1" gt "$2"
+}
