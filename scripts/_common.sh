5b9d7c2221aa7d3f6892614be0a2745f608e1d41
Félix Piédallu <felix@piedallu.me>
2024-02-06 15:51:31 +0100

    Manifest v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-pkg_dependencies="postgresql postgresql-client postgresql-contrib postgis postgresql-postgis-scripts postgresql-postgis argon2 imagemagick webp gifsicle jpegoptim optipng pngquant file"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
