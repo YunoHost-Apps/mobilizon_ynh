7c32f72f5ffc7307e0cb20ba17ac22698e9b244c
Robles Rodolphe <rodolphe.robles@sfr.fr>
2024-12-15 21:45:24 +0100

    Update _common.sh
    archtitecture

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,6 +9,7 @@ ynh_user="${app}_notifs"
 MOBILIZON_CONFIG_PATH="$install_dir/config.exs"
 
 debian=$(lsb_release --codename --short)
+arch=$(uname -m)
 
 #=================================================
 # PERSONAL HELPERS
